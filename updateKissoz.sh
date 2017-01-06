c45061a3a42bda0895318d0a2d6c08371508222b
Svend Stave <Svend@Stave.dk>
2017-01-06 18:07:26 +0100

    Changed github URL to get the raw files https://raw.githubusercontent.com/...

--- updateKissoz.sh
+++ updateKissoz.sh
@@ -2,17 +2,17 @@
 
 ##############################################################################
 ## Get this file 
-wget https://github.com/oz1ekd/rpi-kissoz-dk/blob/master/Kissoz-install-info.txt
+wget https://raw.githubusercontent.com/oz1ekd/rpi-kissoz-dk/master/Kissoz-install-info.txt
 
 ##############################################################################
 ## add some helper scripts and files in /home/pi
 ## when logging in you automatically use /home/pi
 
-wget https://github.com/oz1ekd/rpi-kissoz-dk/blob/master/doscreen.sh
+wget https://raw.githubusercontent.com/oz1ekd/rpi-kissoz-dk/master/doscreen.sh
 chmod 755 doscreen.sh
 
-wget https://github.com/oz1ekd/rpi-kissoz-dk/blob/master/fwUpload.sh
+wget https://raw.githubusercontent.com/oz1ekd/rpi-kissoz-dk/master/fwUpload.sh
 chmod 755 fwUpload.sh
 
-wget https://github.com/oz1ekd/rpi-kissoz-dk/blob/master/resetKissoz.sh
+wget https://raw.githubusercontent.com/oz1ekd/rpi-kissoz-dk/master/resetKissoz.sh
 chmod 755 resetKissoz.sh
