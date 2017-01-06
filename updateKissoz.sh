da4bd0fa405e81d5fdf8299560786ce363d79c1d
Svend Stave <Svend@Stave.dk>
2017-01-06 18:15:00 +0100

    added rm -f files before getting updated files from github

--- updateKissoz.sh
+++ updateKissoz.sh
@@ -2,17 +2,22 @@
 
 ##############################################################################
 ## Get this file 
+
+rm -f Kissoz-install-info.txt
 wget https://raw.githubusercontent.com/oz1ekd/rpi-kissoz-dk/master/Kissoz-install-info.txt
 
 ##############################################################################
 ## add some helper scripts and files in /home/pi
 ## when logging in you automatically use /home/pi
 
+rm -f doscreen.sh
 wget https://raw.githubusercontent.com/oz1ekd/rpi-kissoz-dk/master/doscreen.sh
 chmod 755 doscreen.sh
 
+rm -f fwUpload.sh
 wget https://raw.githubusercontent.com/oz1ekd/rpi-kissoz-dk/master/fwUpload.sh
 chmod 755 fwUpload.sh
 
+rm -f resetKissoz.reset.sh
 wget https://raw.githubusercontent.com/oz1ekd/rpi-kissoz-dk/master/resetKissoz.sh
 chmod 755 resetKissoz.sh
