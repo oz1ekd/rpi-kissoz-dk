6af2e3f18abea24de0e16efaa80c5ed524a40479
Svend Stave <Svend@Stave.dk>
2017-01-06 18:01:30 +0100

    added get Kissoz-install-info file

--- updateKissoz.sh
+++ updateKissoz.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 ##############################################################################
+## Get this file 
+wget https://github.com/oz1ekd/rpi-kissoz-dk/blob/master/Kissoz-install-info.txt
+chmod 755 resetKissoz.sh
+
+##############################################################################
 ## add some helper scripts and files in /home/pi
 ## when logging in you automatically use /home/pi
 
